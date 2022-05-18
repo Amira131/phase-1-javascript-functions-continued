18424100795d3adaf763df8f7540181975085c83
Amira de Moya <demoyaamira@gmail.com>
2022-05-18 00:45:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code your solution here
+
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+saturdayFun("bathe my dog")
+
+function mondayWork(activity = "go to the office"){ 
+    return `This Monday, I will ${activity}.`
+}
+
+mondayWork("work from home")
+
+function wrapAdjective(pizas='*'){
+    return function (Ad = "Special"){
+        return `You are ${pizas}${Ad}${pizas}!`
+
+
+    }
+
+}
+
+ const encouragingPromptFunction = wrapAdjective("!!!")
+
